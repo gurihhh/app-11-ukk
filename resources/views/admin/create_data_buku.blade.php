cca9ff5760298df01b9052ab17facb2da30ca924
Revalariansah <ariansyahreval5@gmail.com>
2024-02-26 11:13:49 +0700

    push

--- resources/views/admin/create_data_buku.blade.php
+++ resources/views/admin/create_data_buku.blade.php
@@ -1,72 +1,72 @@
 @extends('layout.app')
 @section('konten')
-    <header class="mb-3">
-        <a href="#" class="burger-btn d-block d-xl-none">
-            <i class="bi bi-justify fs-3"></i>
-        </a>
-    </header>
-    <div class="page-heading">
-        <div class="page-title">
-            <div class="row">
-                <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>Tambah Data Buku</h3>
-                </div>
-                <div class="col-12 col-md-6 order-md-2 order-first">
-                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">DataTable</li>
-                        </ol>
-                    </nav>
-                </div>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  </head>
+
+  <body>
+
+    <div class="container" style="margin-top: 80px">
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+          <div class="card">
+            <div class="card-header">
+              TAMBAH BUKU
             </div>
+            <div class="card-body">
+              <form action="simpan-siswa.php" method="POST">
+                
+                <div class="form-group">
+                  <label>id buku</label>
+                  <input type="text" name="id buuku" placeholder="Masukkan id buku" class="form-control">
+                </div>
+
+                <div class="form-group">
+                  <label>id kategori</label>
+                  <input type="text" name="id kategori" placeholder="Masukkan id kategori" class="form-control">
+                </div>
+
+                <div class="form-group">
+                  <label>Judul Buku</label>
+                 <input type="text" name="juduk buku" placeholder="Masukkan Judul Buku" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label>Penulis</label>
+                   <input type="text" name="penulis" placeholder="Masukkan Penulis" class="form-control">
+                  </div>
+
+                  <div class="form-group">
+                    <label>Penerbit</label>
+                   <input type="text" name="penerbit" placeholder="Masukkan Penerbit" class="form-control">
+                  </div>
+
+                  <div class="form-group">
+                    <label>Tahun Terbit</label>
+                   <input type="text" name="tahun terbit" placeholder="Masukkan Tahun Terbit" class="form-control">
+                  </div>
+
+                  <div class="form-group">
+                    <label>Stok</label>
+                   <input type="text" name="stok" placeholder="Masukkan Stok" class="form-control">
+                  </div>
+                
+                <button type="submit" class="btn btn-success">SIMPAN</button>
+                <button type="reset" class="btn btn-warning">RESET</button>
+
+              </form>
+            </div>
+          </div>
         </div>
-        <section class="section">
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">Create Data Buku</h4>
-                </div>
-
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="basicInput">id buku</label>
-                                <input type="text" class="form-control" id="basicInput" placeholder="id buku">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="helpInputTop">id kategori</label>
-                                <input type="text" class="form-control" id="helpInputTop">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="helperText">judul buku</label>
-                                <input type="text" id="helperText" class="form-control" placeholder="judul buku">
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="disabledInput">penulis</label>
-                                <input type="text" class="form-control" id="" placeholder="Name">
-                            </div>
-                            <div class="form-group">
-                                <label for="readonlyInput">penerbit</label>
-                                <input type="text" id="helperText" class="form-control" placeholder="penerbit">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="disabledInput">tahun terbit</label>
-                                <input type="text" class="form-control" id="" placeholder="Alamat">
-                            </div>
-                            <div class="form-group">
-                                <label for="disabledInput">stok</label>
-                                <input type="text" class="form-control" id="" placeholder="stok">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+      </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
 @endsection
