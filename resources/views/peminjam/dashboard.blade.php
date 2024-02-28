1c6c88293672a6059e0ca1a617b4b2ae285b9ac9
Morazxz <126551983+Morazxz@users.noreply.github.com>
2024-02-28 09:33:58 +0700

    test commit

--- resources/views/peminjam/dashboard.blade.php
+++ resources/views/peminjam/dashboard.blade.php
@@ -7,7 +7,7 @@
     </header>
 
     <div class="page-heading">
-        <h3>Halo Peminjam</h3>
+        <h3>Halo Peminjamans</h3>
     </div>
     <div class="page-content">
         <section class="row">
@@ -19,7 +19,10 @@
                                 <h4>Our Locatian</h4>
                             </div>
                             <div class="card-body">
-                                <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.5756517582654!2d106.63685673618363!3d-6.187498721984438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f929162547c7%3A0xbbf35137362e584d!2sSMK%20Negeri%204%20Kota%20Tangerang!5e0!3m2!1sid!2sid!4v1708133287244!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+                                <div><iframe
+                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.5756517582654!2d106.63685673618363!3d-6.187498721984438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f929162547c7%3A0xbbf35137362e584d!2sSMK%20Negeri%204%20Kota%20Tangerang!5e0!3m2!1sid!2sid!4v1708133287244!5m2!1sid!2sid"
+                                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                                        referrerpolicy="no-referrer-when-downgrade"></iframe></div>
                             </div>
                         </div>
                     </div>
